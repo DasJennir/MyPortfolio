6def934b6af72586c36d8166c595063f8299e585
unknown <indiafoxtrot>
2020-12-18 15:18:25 -0500

    huge changes

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -78,6 +78,49 @@ function initSlideShow(slideshow) {
   }, time);
 }
 
-// buttons
+// change color
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'assets/css/main.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'assets/css/green.css'
+	}
+
+	if(mode == 'solar'){
+		document.getElementById('theme-style').href = 'assets/css/solar.css'
+	}
+
+	if(mode == 'dark'){
+		document.getElementById('theme-style').href = 'assets/css/dark.css'
+  }
+  
+  if(mode == 'red'){
+		document.getElementById('theme-style').href = 'assets/css/red.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
 
 
